85ee15ffc263b00274d1da509ad712973fd7b4f0
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2019-01-29 19:18:09 +0100

    add update item feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,29 @@
-import App, {Container} from "next/app";
-import {ApolloProvider} from "react-apollo";
-import withData from '../lib/withData'
-import Page from './../components/Page';
+import App, {Container} from 'next/app';
+import Page from '../components/Page';
+import {ApolloProvider} from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
-    static async getInitialProps({Component, ctx}) {
-        let pageProps= {};
-        if(Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx)
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
         }
-
-        return pageProps.query = ctx.query;
+        pageProps.query = ctx.query;
+        return { pageProps };
     }
     render() {
-        const {Component, apollo, pageProps} = this.props;
+        const { Component, apollo, pageProps } = this.props;
+
         return (
             <Container>
                 <ApolloProvider client={apollo}>
                     <Page>
-                        <Component {...pageProps}/>
+                        <Component {...pageProps} />
                     </Page>
                 </ApolloProvider>
             </Container>
-        )
+        );
     }
 }
 
